362f427cfda24b16147d428940a48bd859a55d27
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2023-07-31 00:29:33 +0300

    Initial commit

---
+++ base/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .models import Room
+
+
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Lets learn Django'},
+#     {'id': 3, 'name': 'Lets learn React'},
+# ]
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_from.html', context)
